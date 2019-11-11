550137f694bfcf864666297837d429fae5a9e8af
Ryan Jacobson <jacobsonryan28@gmail.com>
2019-11-11 16:17:25 -0700

    Cleaned up package files

--- gui_core/src/main.py
+++ gui_core/src/main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 """This module contains the GUI"""
 
 import sys
--- gui_core/src/models.py
+++ gui_core/src/models.py
@@ -2,7 +2,7 @@
 
 from PyQt5 import QtCore
 from PyQt5.QtCore import Qt
-from gui_msgs.msgs import GuiData
+from gui_msgs.msg import GuiData
 
 
 class DataModel(QtCore.QAbstractListModel):
