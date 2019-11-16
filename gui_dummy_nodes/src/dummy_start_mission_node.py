27a3a2a5018a8f755f889c69b4f31ff5e2166559
Ryan Jacobson <jacobsonryan28@gmail.com>
2019-11-16 15:52:16 -0700

    Added start mission dummy node and service

---
+++ gui_dummy_nodes/src/dummy_start_mission_node.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+"""This dummy node pretends that it started a signal."""
+import rospy
+from gui_msgs.srv import StartMission, StartMissionResponse  # pylint: disable=import-error
+
+
+def start_mission(request):
+    """Return true after printing the request."""
+    if request.filepath == "/home/ryanjacobson/testfolder/bad.bpmn":
+        return StartMissionResponse(False, 'it failed')
+    return StartMissionResponse(True, '')
+
+
+def mission_starter():
+    """Start mission starting dummy node."""
+    rospy.init_node('start_mission_dummy')
+    rospy.Service(rospy.get_param('~start_mission_topic'),
+                  StartMission,
+                  start_mission)
+    rospy.spin()
+
+
+if __name__ == "__main__":
+    mission_starter()
